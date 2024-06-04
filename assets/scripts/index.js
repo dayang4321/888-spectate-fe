1b7c334e935af6fa17492cd906a3b2e42212d7b4
Dave <davetherubix@gmail.com>
2024-06-04 05:34:38 +0100

    implement results display in UI

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,5 +5,41 @@ document.addEventListener('DOMContentLoaded', () => {
     event.preventDefault();
     const drawDateInput = document.getElementById('drawDate').value;
     const inputDate = new Date(drawDateInput);
+    const resultsTableBody = document.getElementById('results_list');
+    const drawDates = getLottoDrawDates(inputDate);
+    let hasHighlightedRow = false;
+    resultsTableBody
+      .querySelector('tr.highlight')
+      ?.classList.remove('highlight');
+    resultsTableBody.querySelectorAll('td.fade-in').forEach((cell) => {
+      console.log('removed');
+      cell.classList.remove('fade-in');
+    });
+
+    drawDates.forEach((drawDate, idx) => {
+      const drawDateString = drawDate.toLocaleString('en-IE', {
+        timeZone: 'Europe/Dublin',
+        dateStyle: 'short',
+      });
+      const now = new Date();
+      const pastOrFuture = drawDate < now ? 'Past' : 'Future';
+
+      const row = resultsTableBody.children[idx];
+      if (pastOrFuture === 'Future' && !hasHighlightedRow) {
+        row.classList.add('highlight');
+        hasHighlightedRow = true;
+      }
+
+      const dataCells = row.querySelectorAll('td');
+
+      dataCells[0].textContent = drawDateString;
+      dataCells[1].textContent = pastOrFuture;
+
+      setTimeout(() => {
+        dataCells.forEach((cell) => {
+          cell.classList.add('fade-in');
+        });
+      }, 200 * idx);
+    });
   });
 });
